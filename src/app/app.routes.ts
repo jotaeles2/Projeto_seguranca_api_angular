d76785bf2fa0ef8716f1c2fda38437ee31cca6ee
JOSE LUIZ GONCALVES DA SILVA <joseg.silva@aluno.senac.rio>
2026-07-17 20:51:38 -0300

    angular4

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,9 @@ import { Routes } from '@angular/router';
 import { EpiList } from './epi/epi-list/epi-list';
 import { EpiDetalhe } from './epi/epi-detalhe/epi-detalhe';
 import { EpiForm } from './epi/epi-form/epi-form';
+import { Login } from './auth/login/login';
+import { Unauthorized } from './auth/unauthorized/unauthorized';
+import { authGuard } from './auth/auth-guard';
 
 
 
@@ -9,5 +12,8 @@ export const routes: Routes = [
     { path: '', redirectTo: 'epi', pathMatch: 'full' },
     { path: 'epi', component: EpiList },
     { path: 'epi/detalhe', component: EpiDetalhe },
-    { path: 'epi/cadastro', component: EpiForm }
+    { path: 'epi/cadastro', component: EpiForm },
+    { path: 'login', component: Login },
+    { path: 'unauthorized', component: Unauthorized},
+    { path: 'epi', component: EpiList, canActivate: [authGuard] }
 ];
